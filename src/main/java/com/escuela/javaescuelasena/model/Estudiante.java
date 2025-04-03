8079ccbf3fb8f969d0cb7a234ca90b3405f6dbc2
Andres Araque <aaraqueamaya397@gmail.com>
2025-04-02 20:10:47 -0500

    [ADD] Agregando metodos Setters y Getters

--- src/main/java/com/escuela/javaescuelasena/model/Estudiante.java
+++ src/main/java/com/escuela/javaescuelasena/model/Estudiante.java
@@ -16,4 +16,67 @@ public class Estudiante {
         this.ciudad = ciudad;
         this.estado = estado;
     }
+
+    public Estudiante() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Estudiante{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", edad=" + edad +
+                ", carrera='" + carrera + '\'' +
+                ", ciudad='" + ciudad + '\'' +
+                ", estado='" + estado + '\'' +
+                '}';
+    }
 }
